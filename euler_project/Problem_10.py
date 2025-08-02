e67ce3a3402ab4476823f7228a61bede0f8456b6
masihfallah85 <masih.fallah85@gmail.com>
2025-08-02 19:50:18 +0100

    adding Problem_10

---
+++ euler_project/Problem_10.py
@@ -0,0 +1,36 @@
+#Problem_10
+
+"""
+What's sum of prime numbers below 2 million?
+"""
+
+#Import square root function from  math library
+from math import sqrt
+
+def prime(number):
+
+    """This function checks if a number is prime or not"""
+
+    for i in range(2,int(sqrt(number)) + 1):
+
+        if number % i == 0:
+
+            return False
+        
+    else :
+
+        return True
+    
+#Create a variable for sum of primes
+total = 0
+
+#Loop through 2 to 2 million
+for i in range(2,2_000_000):
+
+    #Check if i is prime
+    if prime(i):
+
+        total += i
+
+#Print the result
+print(f"{total} is sum of all primes from 1 to 2_000_000")
